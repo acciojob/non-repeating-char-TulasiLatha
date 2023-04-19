36f2c0c00606c91552b52e11aa64a8f2827b8812
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-19 17:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function nonRepeat(str){
+	var output;
+var newString="";
+	for(let i=0;i<str.length();i++){
+		for(let j=i+1;j<str.length();j++){
+			if(i!=j && str.charAt(i)!=str.charAt(j)){
+				output=str.charAt(i);
+			}
+		}
+		var input=prompt("Enter a string");
+		alert(ouput);
